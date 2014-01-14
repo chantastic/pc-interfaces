a90916c7e1ae3f93a4cf5cabb1663eb74d944f2d
Michael Chan <mijoch@gmail.com>
2014-01-14 15:43:59 -0800

    Feat(interfaces_footer): Add 'interfaces_footer' for easy footer creation
    https://trello.com/c/zzP5WKVm/12-create-helper-footer
    
    Applications need a simple block helper to provide wrapping footer styles
    
    * Configure Rails engine to ingeck ApplicationHelper into host application
    * Add 'interfaces_footer' helper
    * Add 'helpers' page to docs
    * Add '.code-sample--visible' modifier for isolated code samples
    * Remove '_footer.html' partial

--- app/helpers/interfaces/application_helper.rb
+++ app/helpers/interfaces/application_helper.rb
@@ -1,4 +1,9 @@
 module Interfaces
   module ApplicationHelper
+    def interfaces_footer(&block)
+      content_node = content_tag :div, capture(&block), class: "content"
+
+      content_tag :footer, content_node, class: "site-footer", role: "contentinfo"
+    end
   end
 end
