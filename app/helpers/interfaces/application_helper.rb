17b9b29f551c25ad27a299c3b843e9aebfa11f22
Michael Chan <mijoch@gmail.com>
2014-01-17 10:34:54 -0800

    feature(helpers): add modal rails helper
    https://trello.com/c/81PiMEew/16-create-helper-modal
    
    Modals have complex markup implementation that is subject to change.  A Rails view helper allows the end-developer implemntation to me simpler allow for changes in modal api.
    
    * Add modal helper
    * Add modal_trigger helper
    * Add modal implementation to `/styleguide/helpers` page
    * Add helpers link to `index`

--- app/helpers/interfaces/application_helper.rb
+++ app/helpers/interfaces/application_helper.rb
@@ -5,5 +5,15 @@ module Interfaces
 
       content_tag :footer, content_node, class: "site-footer", role: "contentinfo"
     end
+
+    def modal(modal_id, options = {}, &block)
+      content_tag :div, capture(&block), id: modal_id, class: "reveal-modal #{options[:class]}", data: { reveal: "" }
+    end
+
+    def modal_trigger(modal_id, options = {}, &block)
+      tag = options[:tag] || :span
+
+      content_tag tag, capture(&block), class: "#{options[:class]}", data: { "reveal-id" => modal_id }
+    end
   end
 end
