7fac09988d3ebe503304120b34365d35fd2c2fee
Kevin Thompson <kevin@kevinthompson.info>
2014-02-06 12:03:57 -0800

    Skip controller callbacks in parent applications for interfaces pages.

--- app/controllers/interfaces/pages_controller.rb
+++ app/controllers/interfaces/pages_controller.rb
@@ -2,6 +2,7 @@ require_dependency "interfaces/application_controller"
 
 module Interfaces
   class PagesController < HighVoltage::PagesController
+    skip_filter *_process_action_callbacks.map(&:filter)
     layout 'interfaces/application'
   end
 end
