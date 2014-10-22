b8edb31ed69d309ae2274a3a7fffcf0f50802164
Michael Chan <mijoch@gmail.com>
2014-10-22 09:27:43 -0700

    chore(tabs): add interfaces.tabs feature manifest

--- vendor/assets/javascripts/interfaces/interfaces.js
+++ vendor/assets/javascripts/interfaces/interfaces.js
@@ -1,11 +1,7 @@
 //= require interfaces/interfaces.components
 //= require interfaces/interfaces.modals
+//= require interfaces/interfaces.tabs
 //= require interfaces/interfaces.data_api
 //= require interfaces/dependencies
 //= require interfaces/shims
 //= require interfaces/demos
-//
-//= require interfaces/tab
-//= require interfaces/tab_list
-//= require interfaces/tab_list_item
-//= require interfaces/tab_content
---
+++ vendor/assets/javascripts/interfaces/interfaces.tabs.js
@@ -0,0 +1,4 @@
+#= require ./tab
+#= require ./tab_content
+#= require ./tab_list
+#= require ./tab_list_item
