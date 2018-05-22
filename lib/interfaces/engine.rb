3588e2345773e4855ac76b78f53280db1ef8d551
Michael Chan <mijoch@gmail.com>
2018-05-21 17:51:37 -0700

    attempt to add a funcion for dynamic asset paths

--- lib/interfaces/engine.rb
+++ lib/interfaces/engine.rb
@@ -17,4 +17,22 @@ module Interfaces
       Rails.application.config.assets.precompile += %w(select1-spinner.gif)
     end
   end
+
+  def favicon_asset_paths(app_name)
+    %W(
+      #{app_name}/favicon-16.png
+      #{app_name}/favicon-32.png
+      #{app_name}/favicon-57.png
+      #{app_name}/favicon-76.png
+      #{app_name}/favicon-96.png
+      #{app_name}/favicon-120.png
+      #{app_name}/favicon-128.png
+      #{app_name}/favicon-144.png
+      #{app_name}/favicon-152.png
+      #{app_name}/favicon-180.png
+      #{app_name}/favicon-196.png
+      #{app_name}/favicon-228.png
+      #{app_name}/favicon-svg.svg
+    )
+  end
 end
