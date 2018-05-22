b319e5b90219c6efa4600a30be5d528751c016f4
Michael Chan <mijoch@gmail.com>
2018-05-21 18:21:52 -0700

    add Interfaces.favicon_asset_paths method

--- lib/interfaces.rb
+++ lib/interfaces.rb
@@ -1,4 +1,5 @@
 require 'pco/url'
 require 'interfaces/engine'
+require 'interfaces/favicon_asset_paths'
 require "interfaces/null_person"
 require "interfaces/null_organization"
---
+++ lib/interfaces/favicon_asset_paths.rb
@@ -0,0 +1,19 @@
+module Interfaces
+  def self.favicon_asset_paths(app_name)
+    %W(
+      #{app_name}/favicon-16.png
+      #{app_name}/favicon-32.png
+      #{app_name}/favicon-57.png
+      #{app_name}/favicon-76.png
+      #{app_name}/favicon-96.png
+      #{app_name}/favicon-120.png
+      #{app_name}/favicon-128.png
+      #{app_name}/favicon-144.png
+      #{app_name}/favicon-152.png
+      #{app_name}/favicon-180.png
+      #{app_name}/favicon-196.png
+      #{app_name}/favicon-228.png
+      #{app_name}/favicon-svg.svg
+    )
+  end
+end
