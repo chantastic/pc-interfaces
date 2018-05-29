8fb56db3923e2b4aa980a15315c624132242ef6a
Michael Chan <mijoch@gmail.com>
2018-05-29 13:35:13 -0700

    chore: update deps in test/dummy app

--- lib/interfaces.rb
+++ lib/interfaces.rb
@@ -1,6 +1,4 @@
 module Interfaces
-  VERSION = '3.0.0-alpha.0'
-
   class Engine < ::Rails::Engine
     isolate_namespace Interfaces
 
---
+++ lib/interfaces/version.rb
@@ -0,0 +1,3 @@
+module Interfaces
+  VERSION = '3.0.0-alpha.0'
+end
